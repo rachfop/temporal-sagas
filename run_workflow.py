4ab0ff4f1700b10a33b3294f498426729333cf57
ktenzer <keith.tenzer@gmail.com>
2023-05-08 13:03:56 -0700

    Got working with temporal cloud and mtls. Added Dockerfiles, deployment and other things needed to run on k8s
    Signed-off-by: ktenzer <keith.tenzer@gmail.com>

--- run_workflow.py
+++ run_workflow.py
@@ -1,7 +1,9 @@
 import uuid
+from typing import Optional
+import os
 
 from flask import Flask, render_template, request
-from temporalio.client import Client
+from temporalio.client import Client, TLSConfig
 
 from activities import BookVacationInput
 
@@ -30,7 +32,26 @@ async def book_vacation():
         book_flight_id=flight,
     )
 
-    client = await Client.connect("localhost:7233")
+    if os.getenv('TEMPORAL_MTLS_TLS_CERT') and os.getenv('TEMPORAL_MTLS_TLS_KEY') is not None:
+        server_root_ca_cert: Optional[bytes] = None
+        f = open(os.getenv('TEMPORAL_MTLS_TLS_CERT'), "rb")
+        client_cert = f.read()
+
+        f = open(os.getenv('TEMPORAL_MTLS_TLS_KEY'), "rb")
+        client_key = f.read()
+
+        # Start client
+        client = await Client.connect(
+            os.getenv('TEMPORAL_HOST_URL'),
+            namespace=os.getenv('TEMPORAL_NAMESPACE'),
+            tls=TLSConfig(
+                server_root_ca_cert=server_root_ca_cert,
+                client_cert=client_cert,
+                client_private_key=client_key,
+            ),
+        )
+    else:
+      client = await Client.connect("localhost:7233")  
 
     result = await client.execute_workflow(
         BookWorkflow.run,
@@ -60,4 +81,4 @@ async def book_vacation():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
